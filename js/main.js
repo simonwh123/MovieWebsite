c72dda48cac5a4de859c0bbd51d09e68e767adf9
simonwh123 <simonwhansen123@hotmail.com>
2022-05-24 10:10:38 +0200

    made "vis mere" actually work

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 getMovies();
 
-function getMovies(){
+function getMovies() {
   axios.get('https://feed.entertainment.tv.theplatform.eu/f/jGxigC/bb-all-pas?form=json&range=1-20&byTags=genre:action&byYear=2017&byProgramType=movie')
     .then((response) => {
       console.log(response);
@@ -12,7 +12,7 @@ function getMovies(){
             <div class="well text-center">
               <img src="${movie.plprogram$thumbnails["orig-594x408"].plprogram$url}">
               <h5>${movie.title}</h5>
-              <a onclick="movieSelected('${movie.tdc$imdbId}')" class="btn btn-primary" href="#">Vis mere</a>
+              <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Vis mere</a>
             </div>
           </div>
         `;
@@ -25,47 +25,54 @@ function getMovies(){
     });
 }
 
-function movieSelected(id){
-  sessionStorage.setItem('id', id);
+function movieSelected(id) {
+  sessionStorage.setItem('movieId', id);
   window.location = 'movie.html';
   return false;
 }
 
-function getMovie(){
-  let movieId = sessionStorage.getItem('id');
+function getMovie() {
 
-  axios.get('https://feed.entertainment.tv.theplatform.eu/f/jGxigC/bb-all-pas?form=json&range=1-20&byTags=genre:action&byYear=2017&byProgramType=movie'+movieId)
+  axios.get('https://feed.entertainment.tv.theplatform.eu/f/jGxigC/bb-all-pas?form=json&range=1-20&byTags=genre:action&byYear=2017&byProgramType=movie')
     .then((response) => {
       console.log(response);
-      let movie = response.data.entries;
+      movieId = sessionStorage.getItem('movieId');
+      let movies = response.data.entries;
+      console.log(movieId);
 
-      let output =`
-        <div class="row">
-          <div class="col-md-4">
-            <img src="${movie.plprogram$thumbnails["orig-594x408"].plprogram$url}" class="thumbnail">
+      movies.forEach(element => {
+        if (element.id == movieId) {
+          console.log(element);
+          output = `
+          <div class="row">
+            <div class="col-md-4">
+              <img src="${element.plprogram$thumbnails["orig-594x408"].plprogram$url}" class="thumbnail">
+            </div>
+            <div class="col-md-8">
+              <h2>${element.title}</h2>
+              <ul class="list-group">
+                <li class="list-group-item"><strong>Genre:</strong> ${element.title}</li>
+                <li class="list-group-item"><strong>Released:</strong> ${element.tdc$sortDate}</li>
+                <li class="list-group-item"><strong>Rated:</strong> ${element.plprogram$ratings}</li>
+                <li class="list-group-item"><strong>IMDB Rating:</strong> ${element.tdc$imdbId}</li>
+                <li class="list-group-item"><strong>Director:</strong> ${element.plprogram$personName}</li>
+                <li class="list-group-item"><strong>Actors:</strong> ${element.plprogram$personName}</li>
+              </ul>
+            </div>
           </div>
-          <div class="col-md-8">
-            <h2>${movie.title}</h2>
-            <ul class="list-group">
-              <li class="list-group-item"><strong>Genre:</strong> ${movie.title}</li>
-              <li class="list-group-item"><strong>Released:</strong> ${movie.tdc$sortDate}</li>
-              <li class="list-group-item"><strong>Rated:</strong> ${movie.plprogram$ratings}</li>
-              <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.tdc$imdbId}</li>
-              <li class="list-group-item"><strong>Director:</strong> ${movie.plprogram$personName}</li>
-              <li class="list-group-item"><strong>Actors:</strong> ${movie.plprogram$personName}</li>
-            </ul>
+          <div class="row">
+            <div class="well">
+              <h3>Plot</h3>
+              ${element.description}
+              <hr>
+              <a href="http://imdb.com/title/${element.tdc$imdbId}" target="_blank" class="btn btn-primary">View IMDB</a>
+              <a href="index.html" class="btn btn-default">Go Back To Search</a>
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="well">
-            <h3>Plot</h3>
-            ${movie.description}
-            <hr>
-            <a href="http://imdb.com/title/${movie.tdc$imdbId}" target="_blank" class="btn btn-primary">View IMDB</a>
-            <a href="index.html" class="btn btn-default">Go Back To Search</a>
-          </div>
-        </div>
-      `;
+        `;
+
+        }
+      });
 
       $('#movie').html(output);
     })
